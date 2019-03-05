1ee1edcb532e56f1ad17b8bcca770fe7c54322fd
onurozler <onur.ozler@ug.bilkent.edu.tr>
2019-03-05 22:04:11 +0300

    Test

--- Assets/Scripts/RandomizeAnimationSpeed.cs
+++ Assets/Scripts/RandomizeAnimationSpeed.cs
@@ -11,7 +11,7 @@ public class RandomizeAnimationSpeed : MonoBehaviour
 
         animator.SetFloat("speed", Random.Range(0.5f, 2f));
     }
-
+    // Test
     // Update is called once per frame
     void Update()
     {
